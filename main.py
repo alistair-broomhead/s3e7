6bbcfddcfe728dc231511b02bf0d441492fd9609
Alistair Broomhead <theheadofabroom@hotmail.com>
2012-03-01 20:39:22 +0000

    starting twitter bot

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 __author__ = 'Team # (Alistair Broomhead, x y z)'
 
+from flaskext.oauth import OAuth
+
+
+
 def main():
-    pass
+    oauth = OAuth()
+    twitter = oauth.remote_app('twitter',
+        base_url='http://api.twitter.com/1/',
+        request_token_url='http://api.twitter.com/oauth/request_token',
+        access_token_url='http://api.twitter.com/oauth/access_token',
+        authorize_url='http://api.twitter.com/oauth/authenticate',
+        consumer_key='<your key here>', #TODO#
+        consumer_secret='<your secret here>' #TODO#
+    )
+
 
 if __name__ == "__main__":
     main()
